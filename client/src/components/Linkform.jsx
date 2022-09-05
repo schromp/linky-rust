1c239d09a5bd0f26086b9182d69ae98f43e58b7e
Lennart Koziollek <lennart.koziollek@stud.uni-due.de>
2022-09-05 21:06:25 +0200

    moved alert to component

--- client/src/components/Linkform.jsx
+++ client/src/components/Linkform.jsx
@@ -1,5 +1,8 @@
 import "../App.css";
+import Alert from "./Alert";
 
+
+//This has to be reworked. shortlink is scaling wrong
 function Linkform() {
   return (
     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
@@ -29,24 +32,7 @@ function Linkform() {
           </label>
         </div>
 
-        <div class="alert alert-error shadow-lg">
-          <div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="stroke-current flex-shrink-0 h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-            <span>You can only use Characters a-z 0-9.</span>
-          </div>
-        </div>
+        <Alert/>
 
         <div className="form-control mt-6">
           <button className="btn btn-primary">Generate</button>
